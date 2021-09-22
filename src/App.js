d389fc459e87a941f5655222a45ae0eab686b82d
chillydk147 <dmq@outlook.ie>
2021-09-22 12:32:38 +0100

    #3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data,
+      qrcodeZoomed: false
+    };
+    this.qrcodeZoom = this.qrcodeZoom.bind(this);
+    this.qrcodeUnZoom = this.qrcodeUnZoom.bind(this);
+  }
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  cleanPhone(phone){
+    return phone.replace(/[^\d.-/+]/g, '');
+  }
+
+  getParams() {
+    var params = {};
+    var parser = document.createElement("a");
+    parser.href = window.location.href;
+    var query = parser.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
+  }
+
+  qrcodeZoom(){
+    this.setState({
+      qrcodeZoomed: true   
+    });
+  }
+
+  qrcodeUnZoom(){
+    this.setState({
+      qrcodeZoomed: false   
+    });
+  }
+  render() {
+    const { baseDir } = this.props;
+    const params = this.getParams();
+    const person = params["p"] === undefined ? "sarah-colgan" : params["p"];
+    let p = this.state.data.filter((p) => p.Photo === person)[0];
+    if(p === undefined){
+        p = this.state.data.filter((p) => p.Photo === "sarah-colgan")[0];
+    }
+    let name = p.Prefix +" "+ p.FirstName +" "+ p.MiddleNames +" " + p.LastName;
+    name = p.Suffix !== "" ? name + ", " + p.Suffix : name;
+
+    document.title = name;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <div id="qrcode-mask" className={
+            this.state.qrcodeZoomed
+              ? ""
+              : "hide"
+          }>
+          <img alt={name + " - QR Code"} src={baseDir+"/images/qrcodes/" + p.Photo + ".jpg"} onClick={this.qrcodeUnZoom}/>
+        </div>
+        abc
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import data from "./data/data.json";
+import "./styles.css";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App
+    baseDir="https://www.diaceutics.co.uk/card/"
+    data={data}
+    ref={(ourComponent) => {
+      window.ourComponent = ourComponent;
+    }}
+  />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
