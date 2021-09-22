6c0013e4383b05155e0efdcf13eb934ef060434f
chillydk147 <dmq@outlook.ie>
2021-09-22 12:45:14 +0100

    #5

--- src/App.js
+++ src/App.js
@@ -63,7 +63,92 @@ class App extends React.Component {
           }>
           <img alt={name + " - QR Code"} src={baseDir+"/images/qrcodes/" + p.Photo + ".jpg"} onClick={this.qrcodeUnZoom}/>
         </div>
-        abc1
+        <div id="portrait-container">
+          <div id="P1">
+            <a href="https://www.diaceutics.com/"><img alt="Diaceutics" className="logo" src={baseDir+"images/logo.png"}/></a>
+          </div>
+          <div id="P2">
+            {p.LinkedinUrl !== "" ? (
+              <a className="name" href={p.LinkedinUrl}><img alt={name} className="photo" src={baseDir+"images/colleagues/" + p.Photo + ".jpg"}/></a>
+            ) : (
+              <img alt={name} className="photo" src={baseDir+"images/colleagues/" + p.Photo + ".jpg"}/>
+            )}
+            {p.LinkedinUrl !== "" ? (
+              <a className="name" href={p.LinkedinUrl}>{name}</a>
+            ) : (
+              <span className="name">{name}</span>
+            )}
+            <span className="info position">{p.JobTitle}</span>
+            {p.Department !== "" ? (
+              <span className="info position">{p.Department}</span>
+            ) : null}
+            {p.Country !== "" ? (
+              <span className="info position">{p.Country}</span>
+            ) : null}
+            <a className="info email" href={"mailto:" + p.Email}>
+              {p.Email}
+            </a>
+            {p.Phone1 !== "" ? (
+              <span className="info">
+                <a className="phone" href={"tel:"+this.cleanPhone(p.Phone1)}>{p.Phone1}</a>
+                {p.Phone2 !== "" ? (
+                  <span>
+                    {" "}
+                    |{" "}
+                    <a className="phone" href={"tel:"+this.cleanPhone(p.Phone2)}>{p.Phone2}</a>
+                  </span>
+                ) : null}
+              </span>
+            ) : null}
+            <a className="button" href={baseDir+"/vcards/" + p.Photo + ".vcf"}>Add To Contacts</a><br />
+            <img alt={name + " - QR Code"} className="qrcode" src={baseDir+"/images/qrcodes/" + p.Photo + ".jpg"} onClick={this.qrcodeZoom}/>
+          </div>
+        </div>
+        <div id="landscape-container">
+          <div id="L1">
+            <a href="https://www.diaceutics.com/"><img alt="Diaceutics" className="logo" src={baseDir+"images/logo.png"}/></a>
+            {p.LinkedinUrl !== "" ? (
+              <a className="name" href={p.LinkedinUrl}><img alt={name} className="photo" src={baseDir+"images/colleagues/" + p.Photo + ".jpg"}/></a>
+            ) : (
+              <img alt={name} className="photo" src={baseDir+"images/colleagues/" + p.Photo + ".jpg"}/>
+            )}            
+          </div>
+          <div id="L2">
+            {p.LinkedinUrl !== "" ? (
+              <a className="name" href={p.LinkedinUrl}>{name}</a>
+            ) : (
+              <span className="name">{name}</span>
+            )}
+            <span className="info position">{p.JobTitle}</span>
+            {p.Department !== "" ? (
+              <span className="info position">{p.Department}</span>
+            ) : null}
+            {p.Country !== "" ? (
+              <span className="info position">{p.Country}</span>
+            ) : null}
+            <a className="info email" href={"mailto:" + p.Email}>
+              {p.Email}
+            </a>
+            {p.Phone1 !== "" ? (
+              <span className="info">
+                <a className="phone" href={"tel:"+this.cleanPhone(p.Phone1)}>{p.Phone1}</a>
+                {p.Phone2 !== "" ? (
+                  <span>
+                    {" "}
+                    |{" "}
+                    <a className="phone" href={"tel:"+this.cleanPhone(p.Phone2)}>
+                      {p.Phone2}
+                    </a>
+                  </span>
+                ) : null}
+              </span>
+            ) : null}
+          </div>
+          <div id="L3">
+            <img alt={name + " - QR Code"} className="qrcode" src={baseDir+"/images/qrcodes/" + p.Photo + ".jpg"} onClick={this.qrcodeZoom}/>
+            <a className="button" href={baseDir+"/vcards/" + p.Photo + ".vcf"}>Add To Contacts</a>
+          </div>
+        </div>
       </div>
     );
   }
